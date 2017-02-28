b290353afac30db1e4c5cb1197223be19a4295ab
Aymeric <Aymeric@AYMERIC-PC>
2017-02-28 23:05:37 +0100

    Connexion à la bdd ne fonctionne pas encore

--- GSB/DAOFactory.cs
+++ GSB/DAOFactory.cs
@@ -13,31 +13,42 @@ namespace GSB
 
        public DAOFactory()
         {
-            this.Connexion();
+            this.InitConnexion();
         }
 
-        public void Connexion()
+        public void InitConnexion()
         {
             string connexion = "SERVER=127.0.0.1; DATABASE=gsb_c#; UID=root; PASSWORD=root";
             this.connection = new MySqlConnection(connexion);
             Console.Write("On est co à la base");
+            this.execSqlWrite("INSERT INTO `comptes` (`ID`, `Login`, `Mdp`, `Type`) VALUES(NULL, 'admin', 'admin', 'admin')");
         }
 
-        public void Deconnexion()
+        public void deconnexion()
         {
             this.connection.Close();
         }
 
-        public void ExecSqlRead(String req)
+        public void execSqlRead(String req)
         {  //à remplir 
-            try
-            {
 
-            }
-            catch
-            {
+            MySqlCommand cmd = this.connection.CreateCommand(); 
+            //Création d'une commande SQL en fonction de l'objet connection
+            cmd.CommandText = req; //on entre la req sql
+            cmd.ExecuteNonQuery(); //on ex la req
 
-            }
+            this.deconnexion();
+            
+        }
+
+        public void execSqlWrite(String req)
+        {
+            MySqlCommand cmd = this.connection.CreateCommand();
+            //Création d'une commande SQL en fonction de l'objet connection
+            cmd.CommandText = req; //on entre la req sql
+            cmd.ExecuteNonQuery(); //on ex la req
+
+            this.deconnexion();
         }
 
     }
